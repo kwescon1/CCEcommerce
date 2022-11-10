5a698bfa02db8a6fede98457d0b07ee4cc3bcb24
Odame <kwesi.danquah@daaseng.com>
2022-11-10 18:01:25 +0000

    macro functions for API responses

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +28,34 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        Schema::defaultStringLength(191);
+        JsonResource::withoutwrapping();
+
+        Response::macro('success', function ($data) {
+            return response()->json([
+                'data' => $data ?: null,
+            ]);
+        });
+
+        Response::macro('created', function ($data) {
+            return response()->json([
+                'data' => $data ?: null,
+            ], \Illuminate\Http\Response::HTTP_CREATED);
+        });
+
+        Response::macro('notfound', function ($error) {
+            return response()->json([
+                'error' => $error,
+            ], \Illuminate\Http\Response::HTTP_NOT_FOUND);
+        });
+
+        Response::macro('error', function ($error, $statusCode = \Illuminate\Http\Response::HTTP_INTERNAL_SERVER_ERROR) {
+            return response()->json([
+                'error' => $error,
+            ], $statusCode);
+        });
+
+        // Model::shouldBeStrict(!$this->app->isProduction());
     }
 }
